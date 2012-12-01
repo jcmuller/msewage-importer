fe2e7fb8f6588fc987164f79f8041bed18f7dc4f
Juan C. Müller <jcmuller@gmail.com>
2012-12-01 16:46:26 -0500

    Add options

--- lib/msewage-importer/cli.rb
+++ lib/msewage-importer/cli.rb
@@ -19,26 +19,29 @@ module Msewage::Importer
     def setup
       set_program_name
       process_command_line_options
-    rescue GetoptLong::MissingArgument, GetoptLong::InvalidOption
+    rescue GetoptLong::MissingArgument, GetoptLong::InvalidOption, GetoptLong::AmbiguousOption
       puts
-      show_help_and_exit
-    end
-
-    def run
-      puts "Sh*t works"
-      # Importer.import(options)
+      puts "Run #{program_name} --help to see the available options"
+      exit
     end
 
     private
 
     attr_reader :options
 
+    def run
+      Importer.new(options).import
+    end
+
     def options_possible
       [
-        ['--config',  '-c', GetoptLong::REQUIRED_ARGUMENT, 'Override the configuration file location'],
-        ['--help',    '-h', GetoptLong::NO_ARGUMENT, 'Show this text'],
-        ['--verbose', '-v', GetoptLong::NO_ARGUMENT, ''],
-        ['--version', '-V', GetoptLong::NO_ARGUMENT, 'Show version info'],
+        ['--config',  '-c', GetoptLong::REQUIRED_ARGUMENT, 'Override the configuration file'],
+        ['--help',    '-h', GetoptLong::NO_ARGUMENT,       'Show this text'],
+        ['--source',  '-s', GetoptLong::REQUIRED_ARGUMENT, 'Source file with data to import'],
+        ['--type',    '-t', GetoptLong::REQUIRED_ARGUMENT, 'Type of defecation site sources to import'],
+        ['--types',   '-T', GetoptLong::NO_ARGUMENT,       'Show type of defecation sites available'],
+        ['--verbose', '-v', GetoptLong::NO_ARGUMENT,       'Be extremely verbose'],
+        ['--version', '-V', GetoptLong::NO_ARGUMENT,       'Show version info'],
       ]
     end
 
@@ -56,12 +59,36 @@ module Msewage::Importer
         when '--version'
           puts version_info
           exit
+        when '--types'
+          show_types_and_exit
+        when '--type'
+          options[:type] = arg
+        when '--source'
+          options[:source] = arg
         end
       end
     end
 
     def cli_options
       @cli_options ||= GetoptLong.new(*options_possible.map{ |o| o.first(3) })
+    end
+
+    def types_supported
+      %w(
+        industrial_waste
+        latrine
+        manure
+        open_defecation_site
+        open_sewage
+        septic_tank
+        sewage_outflow
+        toilet
+      )
+    end
+
+    def show_types_and_exit
+      puts types_supported * ", "
+      exit
     end
 
     def version_info
